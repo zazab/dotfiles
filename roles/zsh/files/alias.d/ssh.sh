56fadfffed8542d2df1fc409284431df320e12cd
Evgeny Persienko <epersienko@ivi.ru>
2018-03-27 14:28:58 +0300

    updated zsh role

--- roles/zsh/files/alias.d/ssh.sh
+++ roles/zsh/files/alias.d/ssh.sh
@@ -12,17 +12,20 @@ alias -g Z='zsh'
 alias -s L='ssh-helper 192.168. .L "" ""'
 alias -s P='ssh-helper 172.16. .P "" ""'
 alias -s e='ssh-helper ngs.ru. .e "" ""'
-alias -s s='ssh-helper "" .s .s ""'
+alias -s s='ssh-helper "" .s .dev.ivi.ru ""'
+alias -s i='ssh-helper "" .i .dev.ivi.ru ""'
+alias -s n='ssh-helper "" .n .at.netstream.ru ""'
 alias -s ru='ssh-helper "" .ru .ru ""'
-alias -s p='ssh-helper "" .p .in.ngs.ru ""'
+alias -s mk='ssh-helper "" .mk .epersienko.notkube.dev.ivi.ru ""'
+alias -s k='ssh-helper "" .k .notkube.dev.ivi.ru ""'
 alias -s x='ssh-helper "" .x "" ""'
 alias -s R='ssh-helper "" .R "" "root"'
 
 function ssh-helper() {
     local prefix="$1"
-    local user="$4"
     local remove_suffix="$2"
     local suffix="$3"
+    local user="$4"
     local host="$5"
     shift 5
     
