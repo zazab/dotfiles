b483c3ddf171852de38681a9af45352ac23e6da5
Evgeny Persienko <persienkoev@gmail.com>
2021-04-14 15:42:50 +0300

    добавил роль для установки brew пакетов

--- roles/zsh/files/profile.d/zsh_autosuggestions.zsh
+++ roles/zsh/files/profile.d/zsh_autosuggestions.zsh
@@ -1,7 +1,6 @@
 export ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd completion)
 export ZSH_AUTOSUGGEST_USE_ASYNC="on"
 
-bindkey "\eC" autosuggest-clear
+bindkey '^[^?' autosuggest-clear
 bindkey "\eS" autosuggest-toggle
-bindkey '^[^?'
 
