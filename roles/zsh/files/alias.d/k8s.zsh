8dbfcffac55a8f30479b18d90b852d956294cbb6
Evgeniy Persienko <evpersienko@avito.ru>
2022-11-18 10:25:38 +0300

    updated zsh configs

--- roles/zsh/files/alias.d/k8s.zsh
+++ roles/zsh/files/alias.d/k8s.zsh
@@ -5,15 +5,72 @@ alias k="kubectl"
 alias kl="kubectl logs"
 alias kt="kubetail"
 alias ke="kubectl exec -ti"
+
+# get aliases
 alias kgp="kubectl get po"
-alias kgdpl="kubectl get deploy"
+alias kgd="kubectl get deploy"
 alias kgns="kubectl get ns"
-alias kdp="kubectl describe po"
 alias kgn="kubectl get no"
-alias kdn="kubectl describe no"
+alias kgn_taints="kubectl get no -o go-template-file=/Users/evpersienko/.kube/tpls/node_with_taints.tpl"
+alias kgn_labels="kubectl get no -o go-template-file=/Users/evpersienko/.kube/tpls/node_with_labels.tpl"
+alias kgs="kubectl get svc"
+alias kgcm="kubectl get cm"
+alias kgsec="kubectl get secret"
+alias kgrs="kubectl get rs"
+alias kgrb="kubectl get rolebinding"
+alias kgro="kubectl get role"
+alias kgcrb="kubectl get clusterrolebinding"
+alias kgcro="kubectl get clusterrole"
+alias kgcrd="kubectl get crd"
+alias kgsa="kubectl get sa"
+alias kgi="kubectl get ing"
+alias kgds="kubectl get ds"
 alias kge='kubectl get events --sort-by='\''{.lastTimestamp}'\'
 
-alias ksc="avito kubectl use-context"
+alias kdp="kubectl describe po"
+alias kdd="kubectl describe deploy"
+alias kdn="kubectl describe no"
+alias kdns="kubectl describe ns"
+alias kds="kubectl describe svc"
+alias kdcm="kubectl describe cm"
+alias kdsec="kubectl describe secret"
+alias kdrs="kubectl describe rs"
+alias kdrb="kubectl describe rolebinding"
+alias kdro="kubectl describe role"
+alias kdcrb="kubectl describe clusterrolebinding"
+alias kdcro="kubectl describe clusterrole"
+alias kdcrd="kubectl describe crd"
+alias kdsa="kubectl describe sa"
+alias kdi="kubectl describe ing"
+alias kdds="kubectl describe ds"
+
+kube_set_context() {
+    context="$1"
+
+    rm -f "$HOME/.kube/config"
+    if [[ "$context" == "minikube" ]]; then
+        cp "$HOME/.kube/config.minikube" "$HOME/.kube/config"
+    else
+        cp "$HOME/.kube/config.avito" "$HOME/.kube/config"
+    fi
+
+    kubectl config use-context $context
+    #avito kubectl use-context $context
+}
+alias ksc="kube_set_context"
+
+kube_sync_config() {
+    set -x
+    rm -f "$HOME/.kube/config"
+    cp "$HOME/.kube/config.avito" "$HOME/.kube/config"
+    avito kubectl sync-config
+    rm -f "$HOME/.kube/config.avito"
+    cp "$HOME/.kube/config" "$HOME/.kube/config.avito"
+    set +x
+}
+alias ksync="kube_sync_config"
+
+#alias ksc="avito kubectl use-context"
 
 kube_set_namespace() {
     kubectl get namespace $1 > /dev/null;
